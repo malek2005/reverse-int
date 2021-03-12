0553b345693d2e740b0a5455e41af275c5f5ec23
malek2005 <malek2005@list.ru>
2021-03-12 20:58:54 +0200

    feat: solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (n) 
+{    
+    if ( n<0 ) {
+        n = n*-1
+    }
+     else { n = n}
+     n=n.toString();
+ return n.split('').reverse().join('');
+    }
   
-}
+
+
+
